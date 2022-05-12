4abf70a27f4d5a8b22ddc59bde772aa348ad3563
adeniasfilho <adeniasfilho.n1@gmail.com>
2022-05-12 17:19:22 -0300

    criado serviço para acessar recursos do Correntista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
 import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MovimentacaoNewComponent } from './components/movimentacao-new/moviment
     MovimentacaoNewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
